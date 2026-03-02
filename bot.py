b1e72b43e7f80c980f51e8d7a38fe09fd4952811
mashannsome <131888090+mashannsome@users.noreply.github.com>
2026-03-03 01:21:27 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,58 @@
+import requests
+import pandas as pd
+import ta
+import time
+import os
+
+TOKEN = os.getenv("8768274861:AAHG-n6c3CtKQCJ9DAhUH9XqR3RHxq8amtY")
+CHAT_ID = os.getenv("6439008386")
+
+def send_telegram(message):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    data = {"chat_id": CHAT_ID, "text": message}
+    requests.post(url, data=data)
+
+def get_price():
+    url = "https://api.gold-api.com/price/XAUUSD"
+    r = requests.get(url)
+    return r.json()["price"]
+
+prices = []
+
+def check_signal():
+    global prices
+    
+    price = get_price()
+    prices.append(price)
+
+    if len(prices) > 100:
+        prices.pop(0)
+
+    df = pd.DataFrame(prices, columns=["close"])
+    
+    if len(df) < 30:
+        return
+
+    df['ema9'] = ta.trend.ema_indicator(df['close'], window=9)
+    df['ema21'] = ta.trend.ema_indicator(df['close'], window=21)
+    df['rsi'] = ta.momentum.rsi(df['close'], window=14)
+
+    last = df.iloc[-1]
+    entry = round(last['close'],2)
+
+    if last['ema9'] > last['ema21'] and last['rsi'] > 55:
+        sl = round(entry - 3,2)
+        tp = round(entry + 6,2)
+        send_telegram(f"🔥 BUY XAUUSD\nEntry: {entry}\nSL: {sl}\nTP: {tp}")
+
+    if last['ema9'] < last['ema21'] and last['rsi'] < 45:
+        sl = round(entry + 3,2)
+        tp = round(entry - 6,2)
+        send_telegram(f"🔥 SELL XAUUSD\nEntry: {entry}\nSL: {sl}\nTP: {tp}")
+
+while True:
+    try:
+        check_signal()
+        time.sleep(60)
+    except:
+        time.sleep(60)
